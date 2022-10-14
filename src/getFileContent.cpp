bcccff54c0af699c819b6cbfb8e4b7f855c19126
Moritz Molch <mail@moritzmolch.de>
2022-10-15 00:28:42 +0200

    Improved getFileContent

--- src/getFileContent.cpp
+++ src/getFileContent.cpp
@@ -13,18 +13,29 @@ std::vector<char> getFileContent(std::string_view path)
     std::ifstream file(path.data(), std::ios::binary);
     if (!file)
     {
-        throw std::runtime_error { std::string("Failed to open file: ") + path.data() };
+        throw std::runtime_error { std::string("Failed to open file for reading: ") + path.data() };
     }
 
-    // The only way to know how much data can be read is by reading the file!
-    file.ignore( std::numeric_limits<std::streamsize>::max() );
-    std::streamsize length = file.gcount();
-    file.clear(); // Since ignore will have set eof.
-    file.seekg( 0, std::ios_base::beg );
+    // in_avail() returns the number of characters available for reading before the buffer must be refilled
+    auto available = file.rdbuf()->in_avail();
 
-    std::vector<char> data(length, '\0');
-    file.read(data.data(), data.capacity());
-    data.resize(file.gcount());
+    // use a two-step approach. Try to read as much as possible directly into the returned vector first
+    std::vector<char> data(available, '\0');
+
+    if (file.read(data.data(), data.size()))
+    {
+        // There's still more data. Use a buffer.
+        // Use the previous available size (should be the buffer size of the rdbuf() backing the stream)
+        std::vector<char> buffer(available, '\0');
+
+        while (file.read(buffer.data(), buffer.size()))
+        {
+            // Looks initimdating at first but uses memmove under the hood. The rest is constant
+            data.insert(data.begin(), buffer.begin(), std::next(buffer.begin(), file.gcount()));
+        }
+
+        data.insert(data.begin(), buffer.begin(), std::next(buffer.begin(), file.gcount()));
+    }
 
     return data;
 }
--- tests/getFileContent_ctest.cpp
+++ tests/getFileContent_ctest.cpp
@@ -1,12 +1,12 @@
 #include <cassert>
 #include <cstring>
 #include <iostream>
-#include <span>
+#include <string_view>
 
 #include <getFileContent.hpp>
 
 
-const std::span testdata { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." };
+const std::string_view testdata = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 int main(int argc, char const *argv[])
 {
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
 
     assert (static_cast<int>(data.size()) == testdata.size());
 
-    assert (memcmp(data.data(), testdata, testdata.size()) == 0);
+    assert (memcmp(data.data(), testdata.data(), testdata.size()) == 0);
 
     return 0;
 }
